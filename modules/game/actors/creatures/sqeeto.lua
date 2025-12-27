06f4b699a35e8fb466e5be6a0c042c9dd2222944
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-12-27 05:32:38 -0500

    Thrumbles now fight off mosquitos.

--- modules/game/actors/creatures/sqeeto.lua
+++ modules/game/actors/creatures/sqeeto.lua
@@ -15,5 +15,6 @@ prism.registerActor("Sqeeto", function()
       prism.components.Collider(),
       prism.components.Health(3),
       prism.components.Attacker(1),
+      prism.components.SqeetoFaction(),
    }
 end)
