b22ce77e31e0081413707b5c3a6d0ff8cafc94bf
Matthew Blanchard <matthewrblanchard@gmail.com>
2026-01-09 20:04:47 -0500

    Gob moves lights again since sqeetos are pickier now.

--- modules/game/nodes/sqeetotree.lua
+++ modules/game/nodes/sqeetotree.lua
@@ -3,13 +3,14 @@ local SqeetoTree = prism.BehaviorTree.Root:extend "SqeetoTree"
 
 local BT = prism.BehaviorTree
 SqeetoTree.children = {
-   BT.Sequence {
-      BT.Selector {
+   BT.Selector {
+      BT.Sequence {
          prism.nodes.FindEnemyBehavior(false, 3),
-         prism.nodes.FindLightBehavior,
+         prism.nodes.PerformOnBehavior(prism.actions.Attack),
+         prism.nodes.MoveTowardTargetBehavior(1),
       },
       BT.Sequence {
-         prism.nodes.PerformOnBehavior(prism.actions.Attack),
+         prism.nodes.FindLightBehavior,
          prism.nodes.MoveTowardTargetBehavior(1),
       },
    },
