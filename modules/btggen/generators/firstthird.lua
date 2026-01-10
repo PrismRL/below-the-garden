14a5e991f886064edc7f5c5cd40945feeb5a6600
Matthew Blanchard <matthewrblanchard@gmail.com>
2026-01-09 22:02:20 -0500

    Undo last commit.

--- modules/btggen/generators/firstthird.lua
+++ modules/btggen/generators/firstthird.lua
@@ -326,23 +326,11 @@ function FirstThird.generate(generatorInfo, player)
    used[room] = true
    builder:addActor(player, room.center:decompose())
 
-   if depth ~= 6 then
-      room = table.remove(importantRooms, rng:random(#importantRooms))
-      builder:addActor(prism.actors.Prism(), room.center:decompose())
+   room = table.remove(importantRooms, rng:random(#importantRooms))
+   builder:addActor(prism.actors.Prism(), room.center:decompose())
 
-      room = table.remove(importantRooms, rng:random(#importantRooms))
-      builder:addActor(prism.actors.Stairs(), room.center:decompose())
-   else
-      local best, bestD = nil, 0
-      for _, room in ipairs(rooms) do
-         if room.d >= bestD and canSpawnRoom(room) then
-            best = room
-            bestD = room.d
-         end
-      end
-
-      builder:addActor(prism.actors.Crystal(), best.center:decompose())
-   end
+   room = table.remove(importantRooms, rng:random(#importantRooms))
+   builder:addActor(prism.actors.Stairs(), room.center:decompose())
 
    local chestItems = {
       prism.actors.Flippers,
