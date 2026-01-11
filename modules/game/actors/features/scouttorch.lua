50019b5b4a4e69fd818839d64f8572da49a9a59d
LJNIC <liamreadsspam@gmail.com>
2026-01-11 18:04:43 -0400

    Improve fire/scout torch graphics

--- modules/game/actors/features/scouttorch.lua
+++ modules/game/actors/features/scouttorch.lua
@@ -1,12 +1,19 @@
+local Animation = spectrum.Animation
+
+spectrum.registerAnimation("ScoutTorch", function()
+   return Animation({ { index = 286, color = prism.Color4.GOLD }, { index = 287, color = prism.Color4.GOLD } }, 0.5)
+end)
+
 prism.registerActor("ScoutTorch", function()
    return prism.Actor.fromComponents {
       prism.components.Name("ScoutTorch"),
       prism.components.Drawable {
-         index = 240,
+         index = 286,
          color = prism.Color4.ORANGE,
       },
+      prism.components.IdleAnimation("ScoutTorch"),
       prism.components.Position(),
       prism.components.Camp(),
-      prism.components.Light((prism.Color4.YELLOW + prism.Color4.YELLOW) / 1.5, 7, prism.lighteffects.Flicker()),
+      prism.components.Light(prism.Color4.GOLD, 7, prism.lighteffects.Flicker()),
    }
 end)
